6979dc0f5e7b3ca17b70b10b6701ee6604367b80
Heidi Anderson <ander788@purdue.edu>
2017-06-05 18:55:47 -0400

    Added last name and creates paragraph

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ function handleSubmit(ev)
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.firstName.value+' '+f.secondName.value
+
+    const newP = document.createElement('P')
+    const p = document.createTextNode('Fist Name: '+f.firstName.value+'\nLast Name: '+f.secondName.value)
+    newP.appendChild(p);
+    document.body.appendChild(newP)
 }
 
 personForm.addEventListener('submit', handleSubmit)
